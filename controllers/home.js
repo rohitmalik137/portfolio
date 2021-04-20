b2f943cdb2e0aa13ae0c1e21b91e62b337c9d586
Rohit Malik <bpsrohitmalik@gmail.com>
2021-04-20 18:54:26 +0530

    adding alt tags to images for better reachability

--- controllers/home.js
+++ controllers/home.js
@@ -9,15 +9,16 @@ const transporter = nodemailer.createTransport(
   })
 );
 
-var succ_or_fail = null
+var succ_or_fail = null;
 
 exports.sendMail = (req, res, next) => {
   const { uname, email, message } = req.body;
-  transporter.sendMail({
-    to: 'bpsrohitmalik@gmail.com',
-    from: 'bpsrohitmalik@gmail.com',
-    subject: 'Rohit Portfolio | Message',
-    html: `
+  transporter
+    .sendMail({
+      to: 'bpsrohitmalik@gmail.com',
+      from: 'bpsrohitmalik@gmail.com',
+      subject: 'Rohit Portfolio | Message',
+      html: `
           <div style="background-color: #e9e9e9; padding-bottom: 25px">
             <h1 style="text-align:center; font-family: 'Oleo Script', cursive; padding: 10px">
               findRohit
@@ -29,20 +30,19 @@ exports.sendMail = (req, res, next) => {
               <span>message: </span>${message}<br/><br/>
           </div>
         `,
-  })
-  .then(() => {
-    succ_or_fail = 'Your message is received by Rohit Malik'
-    res.redirect('/')
-  })
-  .catch(err => {
-    succ_or_fail = 'An error occured. Please try again!'
-    res.redirect('/')
-  })
+    })
+    .then(() => {
+      succ_or_fail = 'Your message is received by Rohit Malik';
+      res.redirect('/');
+    })
+    .catch((err) => {
+      succ_or_fail = 'An error occured. Please try again!';
+      res.redirect('/');
+    });
 };
 
 exports.homePage = (req, res, next) => {
   res.render('home', {
-    pageTitle: 'Rohit Malik',
-    success_or_failure: succ_or_fail
+    success_or_failure: succ_or_fail,
   });
 };
